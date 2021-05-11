37d9c8e21f8ef82f90230148ea2de4be04e1a758
hxwei88 <45229654+hxwei88@users.noreply.github.com>
2021-05-11 14:51:12 +0800

    Further code organization of newly merged files

---
+++ public/php/admin.php
@@ -0,0 +1,10 @@
+<?php
+    include 'folder_path.php';
+    include $views_path.'admin_header.html';
+    include $views_path.'admin.html';
+    include $views_path.'add_stock.html';
+    include $views_path.'stock.html';
+    include $views_path.'history.html';
+    include $views_path.'admin_footer.html';
+?>
+
