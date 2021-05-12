b0ae57142077e1314d29a621ee425a66e97bf7b9
hxwei88 <45229654+hxwei88@users.noreply.github.com>
2021-05-12 15:55:34 +0800

    POST testing in branch

--- public/php/admin.php
+++ public/php/admin.php
@@ -3,6 +3,7 @@
     include 'insert_product.php';
     include $views_path.'admin_header.html';
     include $views_path.'admin.html';
+    include $views_path.'add_stock.html';
     include $views_path.'admin_footer.html';
 ?>
 
