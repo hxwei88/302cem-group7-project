8fee6addff8709e5863fd3c67b2f234e118f906a
bexuyo <behxuanyong@gmail.com>
2021-05-12 18:20:18 +0800

    image validation

--- public/php/folder_path.php
+++ public/php/folder_path.php
@@ -11,8 +11,5 @@
     
     //filepath in resources
     $images_path = $_SERVER['DOCUMENT_ROOT'].'/302cem-group7-project/public/resources/images/';
-    
-    
-    //test xy
 ?>
 
--- public/php/insert_product.php
+++ public/php/insert_product.php
@@ -7,9 +7,6 @@ include ('db.php');
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
 //    <img src="../resources/images/amogus.jpg" width="500" height="600">
     
-    //specify upload location for image file, still need file type verfication.
-    $target = "../resources/images/" . basename($_FILES['image']['name']);
-
     //assign variable from input form
     $image = $_FILES['image']['name'];
     $isbn = mysqli_real_escape_string($conn, $_POST['isbn_input']);
@@ -21,17 +18,66 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $retail = mysqli_real_escape_string($conn, $_POST['retail_input']);
     $quantity = mysqli_real_escape_string($conn, $_POST['quantity_input']);
     
-    //insert into books table
-    $newProduct = "INSERT INTO books (isbn, name, author, publication_date, description, image, trade_price, retail_price, quantity) VALUES "
-            . "('$isbn', '$name', '$author', '$date','$description', '$image', '$trade', '$retail', '$quantity')";
+    //specify upload location for image file, still need file type verfication.
+    $target = "../resources/images/" . basename($_FILES['image']['name']);
     
-    //Move uploaded file to specified location, /public/resources/images/ in this case, location specified in $target.
-    (move_uploaded_file($_FILES['image']['tmp_name'], $target));
-
-    if (mysqli_query($conn, $newProduct)) {
-        echo'Product Successfully added.';
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target, PATHINFO_EXTENSION));
+    
+    // Check if image file is a actual image or fake image
+    if(isset($_POST["submit"])) {
+      $check = getimagesize($_FILES["image"]["tmp_name"]);
+      if($check !== false) {
+        echo '<script>alert("File is an image.")</script>';
+        $uploadOk = 1;
+      } else {
+        echo '<script>alert("File is not an image.")</script>';
+        $uploadOk = 0;
+      }
+    }
+    
+    // Check if file already exists
+    if (file_exists($target)) {
+      echo '<script>alert("Sorry, file already exists.")</script>';
+      $uploadOk = 0;
+    }
+    
+    // Check file size, 10mb
+    if ($_FILES["image"]["size"] > 10000000) {
+      echo '<script>alert("Sorry, your file is too large.")</script>';
+      $uploadOk = 0;
+    }
+    
+    // Allow certain file formats
+    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+      echo '<script>alert("Sorry, only JPG, JPEG and PNG files are allowed.")</script>';
+      $uploadOk = 0;
+    }
+    
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+      echo '<script>alert("Sorry, your file was not uploaded.")</script>';
+    // if everything is ok, try to upload file
     } else {
-        echo'Product ID already exists!</div>';
+      if (move_uploaded_file($_FILES["image"]["tmp_name"], $target)) {
+        echo '<script>alert("The file has been uploaded.")</script>';
+        
+        //insert into books table
+        $newProduct = "INSERT INTO books (isbn, name, author, publication_date, description, image, trade_price, retail_price, quantity) VALUES "
+                . "('$isbn', '$name', '$author', '$date','$description', '$image', '$trade', '$retail', '$quantity')";
+
+        //Move uploaded file to specified location, /public/resources/images/ in this case, location specified in $target.
+        (move_uploaded_file($_FILES['image']['tmp_name'], $target));
+
+        if (mysqli_query($conn, $newProduct)) {
+            echo '<script>alert("Product Successfully added.")</script>';
+        } else {
+            echo '<script>alert("Product ID already exists!")</script>';
+        }
+    
+      } else {
+        echo '<script>alert("Sorry, there was an error uploading your file")</script>';
+      }
     }
 }
 ?>
