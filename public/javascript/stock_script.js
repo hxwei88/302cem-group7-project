fddcac6b7b71f7d4cd30b252c3b70e8720ca9cfb
joshuabigpp <kaljosis@gmail.com>
2021-05-26 03:22:25 +0800

    mode some changes to UI design on add stock page

--- public/javascript/stock_script.js
+++ public/javascript/stock_script.js
@@ -16,9 +16,8 @@ function append_stock_display(result) {
                 '<img class="card-img-top" style="height: 200px; width: 100%;" src="' + image + '" alt="' + image + '">' +
                 '<div class="card-body" style = "text-align:center;">' +
                 '<h5 class="card-title">' + item.name + '</h5>' +
-                //beh si fan
-                '<button type="button" class="btn btn-success" value = ' + item.isbn + ' onclick ="detailpage(this.value)">Details</button>' +
                 '</div>' +
+                '<button type="button" style="height: 50px; width: 100%; font-size:115%" class="btn btn-info" value = ' + item.isbn + ' onclick ="detailpage(this.value)">Details</button>' +
                 '</div>' +
                 '</div>';
     })
