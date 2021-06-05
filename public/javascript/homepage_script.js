30503d9a1a799bd0650bc50f01bf33905fad3e6b
funme123 <behsifan4896@gmail.com>
2021-06-05 16:08:01 +0800

    Merge origin/behxuanyong into behsifan
    Conflicts:
    	public/javascript/homepage_script.js
    	public/php/homepage.php
    	views/homepage.html

--- public/javascript/homepage_script.js
+++ public/javascript/homepage_script.js
@@ -1,7 +1,40 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+function display(){
+    $("#display_books").html('');
+    var html = '<div class="card h-100">';
+    
+    html+= 'Product image'+
+                        '<img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />'+
+                         'Product details'+
+                        '<div class="card-body p-4">'+
+                           '<div class="text-center">'+
+                                 'Product name'+
+                                '<h5 class="fw-bolder">Fancy Product</h5>'+
+                                 'Product price'+
+                                '$40.00 - $80.00'+
+                            '</div>'+
+                        '</div>'+
+                         'Product actions'+
+                        '<div class="card-footer p-4 pt-0 border-top-0 bg-transparent">'+
+                            '<div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View options</a></div>'+
+                        '</div>';
+    html += '</div>'    
+    $("#display_books").append(html);
+}
+//
+//function request_book_data(data) {
+//
+//
+//    $.ajax({
+//        type: 'post',
+//        url: '/302cem-group7-project/public/php/homepage.php',
+//        data: data,
+//        contentType: false,
+//        cache: false,
+//        processData: false,
+//        success: function (result) {
+////            console.log(result)
+//           
+//        }
+//    });
+//}
+//request_book_data();
--- public/php/homepage.php
+++ public/php/homepage.php
@@ -1,27 +1,5 @@
 <?php
     include 'folder_path.php';
     include $views_path.'homepage.html';
-    include ('db.php');
-    
-    $display = "SELECT * FROM books";
-    $result = mysqli_query($conn, $display);
-    $json = array();
-    
-    if(!empty($result)){
-        while($row = mysqli_fetch_assoc($result))
-        {
-//            $name = $row["name"];
-//            $author = $row["author"];
-//            $image = $row["image"];
-//            $price = $row["price"];
-            
-            $json = $result->fetch_assoc();
-            header("Content-Type: application/json");
-            echo json_encode($json);
-//            echo "isbn: " . $row["isbn"] . " name: " . $row["name"] . " author: " . $row["author"] . " pd: " . $row["publication_date"] . " desc: " . $row["description"] . " img: " . $row["image"] . " tp: " . $row["trade_price"] . " rp: " . $row["retail_price"] . " quantity: " . $row["quantity"];
-        }
-    } else {
-        header("Content-Type: application/json");
-        echo "none";
-    }
+
 ?>
