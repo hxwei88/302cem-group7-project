b626fe53b95b70abe2a7a933dddb0367c69088a2
tanlihshen <45410516+tanlihshen@users.noreply.github.com>
2021-06-05 16:06:58 +0800

    Fixed homepage css, did testing for looping the product cards.

--- public/javascript/homepage_script.js
+++ public/javascript/homepage_script.js
@@ -1,23 +1,25 @@
 function display(){
     $("#display_books").html('');
-    var html = '<div class="card h-100">';
+    var html = '<div class="row">';
     
-    html+= 'Product image'+
-                        '<img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />'+
-                         'Product details'+
-                        '<div class="card-body p-4">'+
-                           '<div class="text-center">'+
-                                 'Product name'+
-                                '<h5 class="fw-bolder">Fancy Product</h5>'+
-                                 'Product price'+
-                                '$40.00 - $80.00'+
+    for (i = 0; i < 5; i++) {
+        html+=  '<div class="col-3 py-2 mb-3">' +
+                '<div class="card h-100">'+            
+                            '<img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />'+
+                            '<div class="card-body p-4">'+
+                               '<div class="text-center">'+
+                                     'Product name'+
+                                    '<h5 class="fw-bolder">Fancy Product</h5>'+
+                                    '$40.00 - $80.00'+
+                                '</div>'+
                             '</div>'+
-                        '</div>'+
-                         'Product actions'+
-                        '<div class="card-footer p-4 pt-0 border-top-0 bg-transparent">'+
-                            '<div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View options</a></div>'+
-                        '</div>';
-    html += '</div>'    
+                            '<div class="card-footer p-4 pt-0 border-top-0 bg-transparent">'+
+                                '<div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View options</a></div>'+
+                            '</div>'+
+                            '</div>'+
+                    '</div>';
+                }
+   html += '</div>'
     $("#display_books").append(html);
 }
 
