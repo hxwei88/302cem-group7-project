735756cafe37db56fec8d88c6d8b074a939dd14a
funme123 <behsifan4896@gmail.com>
2021-06-08 21:34:46 +0800

    2 version cart - post ajax to php and use session to do shopping cart - fully javascript to do shopping cart

--- public/javascript/cart.js
+++ public/javascript/cart.js
@@ -1,46 +1,57 @@
 
 var cart = new Array();
 var totalincart = 0;
-document.getElementById("totalincart").innerHTML = parseInt(sessionStorage.getItem('totalincart'));
+if (localStorage.getItem('totalincart') != null) {
+    document.getElementById("totalincart").innerHTML = parseInt(localStorage.getItem('totalincart'));
+    totalincart = parseInt(localStorage.getItem('totalincart'));
+}
 
+var item = JSON.parse(localStorage.getItem('cart'));
+if (JSON.parse(localStorage.getItem('cart')) != null) {
+    var i;
+    for (i = 0; i < item.length; i++) {
+        cart.push({isbn: item[i].isbn, quantity: item[i].quantity});
+    }
+
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
 function add_to_cart(isbn) {
-    sessionStorage.setItem('totalincart', totalincart.toString());
-    var item = JSON.parse(sessionStorage.getItem(isbn));
-    totalincart = parseInt(sessionStorage.getItem('totalincart'));
-    console.log(totalincart);
+    // set total product number in cart
+    localStorage.setItem('totalincart', totalincart.toString());
+    item = JSON.parse(localStorage.getItem('cart'));
+    totalincart = parseInt(localStorage.getItem('totalincart'));
 
 
     if (item != null) {
-        if (isbn == item['isbn']) {
 
-            var x = item['quantity'] + 1;
-            cart[isbn] = {isbn: isbn, quantity: x};
-            totalincart = totalincart +1;
-            sessionStorage.setItem(isbn, JSON.stringify(cart[isbn]));
-            sessionStorage.setItem('totalincart', totalincart.toString());
 
-        } else {
-            cart[isbn] = {isbn: isbn, quantity: 1};
-            totalincart = totalincart +1;
-            sessionStorage.setItem(isbn, JSON.stringify(cart[isbn]));
-            sessionStorage.setItem('totalincart', totalincart.toString());
+        if (check_cart_item(isbn, item) != true) {
+            cart.push({isbn: isbn, quantity: 1});
         }
+
+        localStorage.setItem('cart', JSON.stringify(cart));
+
     } else {
-        cart[isbn] = {isbn: isbn, quantity: 1};
+        cart.push({isbn: isbn, quantity: 1});
+        localStorage.setItem('cart', JSON.stringify(cart));
 
-
-        totalincart = totalincart +1;
-            sessionStorage.setItem(isbn, JSON.stringify(cart[isbn]));
-            sessionStorage.setItem('totalincart', totalincart.toString());
     }
-    
-    
-    document.getElementById("totalincart").innerHTML = parseInt(sessionStorage.getItem('totalincart'));
-    //sessionStorage.setItem('cart', JSON.stringify(cart['hello']) );
-    
-    //console.log(JSON.parse(JSON.stringify(sessionStorage)));
-     
-     var items = JSON.parse(JSON.stringify(sessionStorage));
-     var v = JSON.parse(items[isbn]);
-     console.log(items)
+    totalincart = totalincart + 1;
+    localStorage.setItem('totalincart', totalincart.toString());
+
+    document.getElementById("totalincart").innerHTML = parseInt(localStorage.getItem('totalincart'));
+
 }
+
+function check_cart_item(isbn, item) {
+    var i;
+    for (i = 0; i < item.length; i++) {
+        if (isbn == item[i].isbn) {
+            item[i].quantity = item[i].quantity + 1;
+            cart[i] = {isbn: isbn, quantity: item[i].quantity};
+            return true;
+        }
+    }
+}
+
+
