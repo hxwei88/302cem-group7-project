adead36f0237aa37139d8f125a72b47bcc02548f
tanlihshen <45410516+tanlihshen@users.noreply.github.com>
2021-06-10 12:44:01 +0800

    Added shopping cart php page

--- public/php/homepage.php
+++ public/php/homepage.php
@@ -6,9 +6,18 @@
     }
     
     //Prints out ALL array session, print_r($_SESSION['cart'); for cart array.
-    echo"<pre>";
-    print_r($_SESSION);
-    echo"</pre>";
+//    echo"<pre>";
+//    print_r($_SESSION);
+//    echo"</pre>";
+//    echo "Size of array: ".sizeof($_SESSION['cart'])."<br>";
+//    echo"<hr>";
+    
+    foreach ($_SESSION['cart'] as $row) {
+        echo $row['isbn'];
+        echo"<br>";
+        echo $row['quantity'];
+        echo "<hr>";
+    }
     
     include 'folder_path.php';
     include $views_path.'public_header.html'; 
