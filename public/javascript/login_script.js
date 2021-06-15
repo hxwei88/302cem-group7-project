7767f026d111eee8a686fa7d16014491e85f5e47
hxwei88 <45229654+hxwei88@users.noreply.github.com>
2021-06-15 20:41:06 +0800

    Redirect changes

--- public/javascript/login_script.js
+++ public/javascript/login_script.js
@@ -15,9 +15,9 @@ function login(event){
             } else {
                if (result.message == "admin") {
                    
-                   window.location.replace("../php/admin.php");
+                   window.location = "../php/admin.php";
                } else {
-                   window.location.replace("../php/homepage.php");
+                   window.location = "../php/homepage.php";
                }
             }
         }
--- public/php/homepage.php
+++ public/php/homepage.php
@@ -1,5 +1,4 @@
 <?php
-    
     include 'folder_path.php';
     include $views_path.'public_header.html'; 
     include $views_path.'homepage.html'; 
